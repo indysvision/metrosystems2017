62d50a05e6602cfd26c62e721cae0b7665303914
Alexandru Marcu <alexandrumarcu@gmail.com>
2017-11-20 16:59:15 +0200

    First v1 wave

---
+++ Catalogus/src/main/java/com/metrotraining/catalogus/persistence/UserRepository.java
@@ -0,0 +1,9 @@
+package com.metrotraining.catalogus.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.metrotraining.catalogus.pojos.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
