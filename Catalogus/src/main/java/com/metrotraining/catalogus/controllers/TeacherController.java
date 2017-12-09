ec1013848415e840940eac4e5199f1bab0d43040
diana.moraru <moraru.diana90@yahoo.com>
2017-12-09 19:27:45 +0200

    new editTeacher div in fragment inviteTeacher, for edit teacher

--- Catalogus/src/main/java/com/metrotraining/catalogus/controllers/TeacherController.java
+++ Catalogus/src/main/java/com/metrotraining/catalogus/controllers/TeacherController.java
@@ -31,9 +31,13 @@ public class TeacherController {
 
 	@RequestMapping(value = "/editTeacher", method = RequestMethod.GET)
 	public String editTeacherPart(@RequestParam(value = "teacherId", required = false) Long id, Model model) {
-		model.addAttribute("mode", "invite");
+		
 		if (id != null) {
 			model.addAttribute("teacher", userList.findById(id));
+			model.addAttribute("mode", "edit");
+		}
+		else {
+			model.addAttribute("mode", "invite");
 		}
 		return "listEditTeacher";
 	}
@@ -58,7 +62,7 @@ public class TeacherController {
 
 		if (id == null) {
 
-			userList.save(new User(name, category, description, emailCreate, null, null, UserRole.TEACHER, 2017, 2017,
+			userList.save(new User(name, category, description, emailCreate, null, null, UserRole.TEACHER, 0, 0,
 					"dummy_pasword", UserStatus.PENDING));
 
 			Email email = new Email();
